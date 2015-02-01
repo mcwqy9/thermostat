f8cabc081a7fffb463a7047b34162e32b5d02846
Matt Wheeler <mwheeler@instructure.com>
2015-01-31 17:21:52 -0700

    stubbed out create action

--- app/controllers/temperature_readings_controller.rb
+++ app/controllers/temperature_readings_controller.rb
@@ -1,5 +1,10 @@
 class TemperatureReadingsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
-    #render text: "Hello World"
+  end
+
+  def create
+    render text: "the spice is flowing"
   end
 end
